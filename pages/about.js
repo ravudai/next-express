58e9c9b56cd6d3496138c471555bf2487e46fe6e
Renga <30759477+ravudai@users.noreply.github.com>
2019-03-15 19:55:39 +0100

    about page added

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+const About = () => (
+  <dive>
+    <h1>About</h1>
+  </dive>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+const Index = () => (
+  <div>
+    <h1>Hello World !!!</h1>
+    <Link href="/about">
+      <a>About</a>
+    </Link>
+  </div>
+);
+
+export default Index;
